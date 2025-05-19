bba633847f06c595f0f75a970df908e67294ded9
Adamputra77 <adamsap8888@gmail.com>
2025-05-19 22:25:19 +0700

    uts

--- uts/src/database/seeders/PelangganSeeder.php
+++ uts/src/database/seeders/PelangganSeeder.php
@@ -14,12 +14,12 @@ class PelangganSeeder extends Seeder
     {
         Pelanggan::insert([
             [
-                'Nama' => 'Yodan Surya Adhe',
+                'Nama' => 'Aulia Rahma',
                 'Alamat' => 'Jl. Telaga Bestari No. 15',
                 'Telepon' => '081234567890',
             ],
             [
-                'Nama' => 'Zhafir Daffa Agnawan',
+                'Nama' => 'Rian Setiawan',
                 'Alamat' => 'Jl. Sudirman Indah No. 16',
                 'Telepon' => '082345678901',
             ],
